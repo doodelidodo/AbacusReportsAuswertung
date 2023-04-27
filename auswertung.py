dcdf4bf8d9bda7ef85e1f966c01a556913d5af13
Meyer Dominic <dominic.meyer@eisenring-kuechenbau.ch>
2023-04-27 16:22:29 +0200

    v202

--- auswertung.py
+++ auswertung.py
@@ -1,27 +1,58 @@
 import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
-# Create a SQLite connection
+#Create a SQLite connection
 conn = sqlite3.connect('ReportAufrufe.sqlite')
 
 # Load data from the 'my_table' table into a Pandas DataFrame
 df = pd.read_sql_query('SELECT * FROM ReportAufrufe', conn)
-print(df.head())
 # Close the SQLite connection
 conn.close()
 
 counts = df['Report'].value_counts().nlargest(10)
 
-print(df["Report"].head())
 
-# Erstelle ein Balkendiagramm
-ax = counts.plot(kind='bar')
+# Datum konvertieren
+df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
 
-# Füge Titel und Achsenbeschriftungen hinzu
-ax.set_title('Häufigkeit von Reports')
-ax.set_xlabel('Report-Art')
-ax.set_ylabel('Häufigkeit')
+# Nur das Datum extrahieren
+df['Date'] = df['TimeStamp'].dt.date
 
-# Zeige das Diagramm an
-plt.show()
+# Gruppieren nach Report und Datum und zählen
+df_grouped = df.groupby([df['Report'], df['Date']]).size().reset_index(name='count')
+
+# Top 10 Berichte auswählen
+top_10_reports = df_grouped.groupby('Report').sum().nlargest(10, 'count').index.values
+df_top_10 = df_grouped[df_grouped['Report'].isin(top_10_reports)]
+unique_reports = df_top_10['Report'].unique().tolist()
+
+# Nach Gesamtzählungen sortieren
+df_top_10_sorted = df_top_10.groupby('Report').sum().sort_values('count', ascending=False).reset_index()
+
+# Erstelle PDF-Datei zum Speichern der Plots
+with PdfPages('plots.pdf') as pdf:
+    # Schleife über die Berichte und Erstellung der Plots
+
+    ax = counts.plot(kind='bar')
+    # Füge Titel und Achsenbeschriftungen hinzu
+    ax.set_title('Häufigkeit von Reports')
+    ax.set_xlabel('Report-Art')
+    ax.set_ylabel('Häufigkeit')
+    
+    
+    # Zeige das Diagramm an
+    plt.tight_layout()
+    pdf.savefig()  # Plot in PDF speichern
+    plt.close()  # Plot schließen, um Speicher freizugeben
+
+    for report in df_top_10_sorted['Report']:
+        # DataFrame für den aktuellen Bericht erstellen
+        df_report = df_top_10[df_top_10['Report'] == report]
+
+        # Plot erstellen
+        ax = df_report.plot.bar(x='Date', y='count', title=report, rot=45)
+        plt.tight_layout()
+        pdf.savefig()  # Plot in PDF speichern
+        plt.close()  # Plot schließen, um Speicher freizugeben
--- writeNewLog.py
+++ writeNewLog.py
@@ -4,22 +4,19 @@ import os
 
 
 def get_filename(path):
-    print(path)
-    print(os.path.basename(path))
     return os.path.basename(path)
-
 
 # r"D:\Abacus\ReportAufrufe\ReportAufrufe.sqlite"
 db_path = r"D:\Abacus\ReportAufrufe\ReportAufrufe.sqlite"
 
 
 # Load the latest timestamp from the database
-conn = sqlite3.connect('ReportAufrufe.sqlite')
+conn = sqlite3.connect(db_path)
 # Load data from the 'my_table' table into a Pandas DataFrame
 latest_timestamp = pd.read_sql_query('SELECT MAX(TimeStamp) FROM ReportAufrufe', conn).iloc[0, 0]
 
 # Load CSV into a Pandas DataFrame D:/Abacus/abac/log/abaengine/rep/run.log
-df = pd.read_csv('log/run.log', usecols=range(15))
+df = pd.read_csv('D:/Abacus/abac/log/abaengine/rep/run.log', usecols=range(15))
 df.columns = df.columns.str.strip()
 df = df.rename(columns={"Report": "Pfad"})
 df["Report"] = df['Pfad'].apply(get_filename)
@@ -31,12 +28,12 @@ df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
 df = df.drop_duplicates(subset=['TimeStamp'])
 
 if latest_timestamp is not None:
-    print(latest_timestamp)
     # Filter DataFrame to only include new entries
     df = df[df['TimeStamp'] > latest_timestamp]
 
-# Create a new SQLAlchemy engine and append the new entries to the SQLite table, replacing duplicates
 if not df.empty:
-    df.to_sql('ReportAufrufe', con=conn, if_exists='append', index=False, chunksize=1000)
+    df.to_sql("ReportAufrufe", con=conn, if_exists='append', index=False, chunksize=1000)
+    conn.close()
 else:
     print('dataframe is empty')
+    conn.close()
