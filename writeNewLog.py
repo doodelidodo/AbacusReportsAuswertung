8fe6f3625f894bdd323f6de7b31d93621b66168c
Meyer Dominic <dominic.meyer@eisenring-kuechenbau.ch>
2023-08-30 09:19:34 +0200

    logging

--- writeNewLog.py
+++ writeNewLog.py
@@ -9,42 +9,44 @@ def get_filename(path):
 # Configure logging
 logging.basicConfig(filename='error_log.txt', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-# r"D:\Abacus\ReportAufrufe\ReportAufrufe.sqlite"
-db_path = r"D:\Abacus\abac\kd\abav\datenbanken\Aufrufe_LongRunning_AbaReports\ReportAufrufe.sqlite"
-
-
-# Load the latest timestamp from the database
-conn = sqlite3.connect(db_path)
-# Load data from the 'my_table' table into a Pandas DataFrame
-latest_timestamp = pd.read_sql_query('SELECT MAX(TimeStamp) FROM ReportAufrufe', conn).iloc[0, 0]
-
-# Load CSV into a Pandas DataFrame D:/Abacus/abac/log/abaengine/rep/run.log
-df = pd.read_csv('D:/Abacus/abac/log/abaengine/rep/run.log', usecols=range(15))
-df_snap = pd.read_csv('D:/Abacus/abac/log/abaengine/prl/run.log', usecols=range(15), index_col=False)
-df = pd.concat([df, df_snap])
-
-df.columns = df.columns.str.strip()
-df = df.replace({'"': ''}, regex=True)
-df = df[~df['Report'].str.endswith('.tmp')]
-df = df[~df.Report.str.contains('8370')]
-df = df.rename(columns={"Report": "Pfad"})
-df["Report"] = df['Pfad'].apply(get_filename)
+try:
+    # r"D:\Abacus\ReportAufrufe\ReportAufrufe.sqlite"
+    db_path = r"D:\Abacus\abac\kd\abav\datenbanken\Aufrufe_LongRunning_AbaReports\ReportAufrufe.sqlite"
 
 
-# Convert the 'TimeStamp' column to a datetime format
-df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
+    # Load the latest timestamp from the database
+    conn = sqlite3.connect(db_path)
+    # Load data from the 'my_table' table into a Pandas DataFrame
+    latest_timestamp = pd.read_sql_query('SELECT MAX(TimeStamp) FROM ReportAufrufe', conn).iloc[0, 0]
 
-# Drop duplicates based on the 'TimeStamp' column
-df = df.drop_duplicates(subset=['TimeStamp'])
+    # Load CSV into a Pandas DataFrame D:/Abacus/abac/log/abaengine/rep/run.log
+    df = pd.read_csv('D:/Abacus/abac/log/abaengine/rep/run.log', usecols=range(15))
+    df_snap = pd.read_csv('D:/Abacus/abac/log/abaengine/prl/run.log', usecols=range(15), index_col=False)
+    df = pd.concat([df, df_snap])
 
-if latest_timestamp is not None:
-    # Filter DataFrame to only include new entries
-    df = df[df['TimeStamp'] > latest_timestamp]
+    df.columns = df.columns.str.strip()
+    df = df.replace({'"': ''}, regex=True)
+    df = df[~df['Report'].str.endswith('.tmp')]
+    df = df[~df.Report.str.contains('8370')]
+    df = df.rename(columns={"Report": "Pfad"})
+    df["Report"] = df['Pfad'].apply(get_filename)
 
-if not df.empty:
-    df.to_sql("ReportAufrufe", con=conn, if_exists='append', index=False, chunksize=1000)
-    conn.close()
-else:
-    print('dataframe is empty')
-    conn.close()
+
+    # Convert the 'TimeStamp' column to a datetime format
+    df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
+
+    # Drop duplicates based on the 'TimeStamp' column
+    df = df.drop_duplicates(subset=['TimeStamp'])
+
+    if latest_timestamp is not None:
+        # Filter DataFrame to only include new entries
+        df = df[df['TimeStamp'] > latest_timestamp]
+
+    if not df.empty:
+        df.to_sql("ReportAufrufe", con=conn, if_exists='append', index=False, chunksize=1000)
+        conn.close()
+    else:
+        print('dataframe is empty')
+        conn.close()
+except Exception as e:
+    logging.error(f"An error occurred: {str(e)}")
